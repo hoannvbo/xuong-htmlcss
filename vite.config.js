74a1db66dce2e348c6e12421be3c89119e64662e
hoannvbo <hoannvph37082@fpt.edu.vn>
2024-03-17 15:14:06 +0700

    update

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                shop: resolve(__dirname, "./shop.html"),
+                detail: resolve(__dirname, "./detail-product.html"),
+                cart: resolve(__dirname, "./cart.html"),
+                checkout: resolve(__dirname, "./checkout.html"),
+                pay: resolve(__dirname, "./pay-product.html"),
+                login: resolve(__dirname, "./login.html"),
+            },
+        },
+    },
+});
